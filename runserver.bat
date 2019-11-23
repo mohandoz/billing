f5d5aa7bede184fc8afbec2856e0223e4a7d988a
mohandoz <mohand.ahmad@outlook.com>
2019-11-23 22:31:08 +0200

    added windows stuff

---
+++ runserver.bat
@@ -0,0 +1,8 @@
+d:\venv\scripts\activate
+
+set ALLOWED_HOSTS=['localhost', '127.0.0.1']
+set DJANGO_SETTINGS_MODULE=config.settings.production
+set DJANGO_SECRET_KEY=dwqkjd@OKR@!_12/f021ir021fsav1_21rflkm
+set DATABASE_URL=postgres://billing:Amman123@localhost:5432/billing
+set DJANGO_ADMIN_URL=MohandozAdmin
+python manage.py runserver
